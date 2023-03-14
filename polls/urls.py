fbd351f2d5ffd667810e44c57da9e3b9102d4d75
alexNicholsonCoding <alex.z.nicholson@gmail.com>
2023-03-14 15:35:59 +0100

    added to urls

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import include, path
+from django.contrib import admin
 
 from . import views
 
 app_name = 'polls'
 urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
